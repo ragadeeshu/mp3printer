e280c8197f0570b82e9d5abc1a6d2954ff5eb6e1
Ragnar Mellbin <ragnar.mellbin@neotechnology.com>
2016-08-26 10:21:36 +0200

    xmlhttp file upload, harden against overlay blinking

--- main.py
+++ main.py
@@ -16,51 +16,45 @@ Messages are output to the terminal for debuggin purposes.
 '''
 clients = connections.Connections()
 juggler = mp3Juggler(clients)
+__UPLOADS__ = "static/songs/"
 class IndexHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     def get(request):
         request.render("index.html")
 
-class WSHandler(tornado.websocket.WebSocketHandler):
+class Upload(tornado.web.RequestHandler):
+    def post(self):
+        fh = open(__UPLOADS__ + self.request.headers.get('Filename'), 'wb')
+        fh.write(self.request.body)
+        self.finish()
 
+class WSHandler(tornado.websocket.WebSocketHandler):
 
     def open(self):
         clients.add_conneciton(self)
-        # print('new connection')
         self.write_message(json.dumps(juggler.get_list()))
 
     def on_message(self, message):
         parsed_json = json.loads(message)
-        # print(parsed_json.keys())
-        parsed_json['file'] = base64.b64decode(parsed_json['file'])
-        parsed_json['path'] = "static/songs/"+parsed_json['filename']
-        with open(parsed_json['path'], "wb") as text_file:
-            text_file.write(parsed_json.pop('file', None))  #this just writes the ASCII for the string
-
+        parsed_json['path'] = __UPLOADS__+parsed_json['filename']
         juggler.juggle(parsed_json)
 
     def on_close(self):
         print('connection closed')
         clients.close_connection(self)
 
-    # def check_origin(self, origin):
-    #     return True
-
 settings = {
     "static_path": os.path.join(os.path.dirname(__file__), "static"),
-    # "cookie_secret": "__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
-    # "login_url": "/login",
-    # "xsrf_cookies": True,
 }
 
 application = tornado.web.Application([
-    (r'/ws', WSHandler), (r'/', IndexHandler)
+    (r'/ws', WSHandler), (r'/', IndexHandler), (r"/upload", Upload),
 ], **settings)
 
 
 if __name__ == "__main__":
     http_server = tornado.httpserver.HTTPServer(application)
-    http_server.listen(8888)
+    http_server.listen(80)
     myIP = socket.gethostbyname(socket.gethostname())
     print('*** Websocket Server Started at %s***' % myIP)
     tornado.ioloop.IOLoop.instance().start()
