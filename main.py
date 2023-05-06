e29a5593aa704920e2a89540e32aa557666d8352
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2023-05-06 13:04:37 +0100

    Handle (and show) upload errors without closing connection.
    Also, clean up CSS somewhat.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import re
 import uuid
 import _thread
 import tempfile
@@ -18,11 +19,18 @@ from mp3Juggler import mp3Juggler
 clients = connections.Connections()
 juggler = mp3Juggler(clients)
 
+ansi_escape = re.compile(r'(\x9B|\x1B\[)[0-?]*[ -/]*[@-~]')
+error_prefix = re.compile(r'^[Ee][Rr][Rr]([Oo][Rr])?:\s*')
+
 def skipper ():
     while True:
         if (input() == "s"):
             print("Skipping...")
             juggler.skip()
+
+def error_message(err):
+    return error_prefix.sub('', ansi_escape.sub('', str(err)))
+
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(request):
@@ -30,18 +38,23 @@ class IndexHandler(tornado.web.RequestHandler):
 
 class Upload(tornado.web.RequestHandler):
     def post(self):
-        filename = self.request.headers.get('Filename')
-        extn = os.path.splitext(filename)[-1]
-        fd, cachename = tempfile.mkstemp(suffix=extn)
-        infile = {'nick':self.request.headers.get('nick'),
-        'filename':filename,
-        'address':self.request.remote_ip,
-        'path':cachename,
-        'mrl':cachename}
-        with os.fdopen(fd, 'wb') as fh:
-            fh.write(self.request.body)
-        self.finish()
-        juggler.juggle(infile)
+        try:
+            filename = self.request.headers.get('Filename')
+            extn = os.path.splitext(filename)[-1]
+            fd, cachename = tempfile.mkstemp(suffix=extn)
+            infile = {'nick':self.request.headers.get('nick'),
+            'filename':filename,
+            'address':self.request.remote_ip,
+            'path':cachename,
+            'mrl':cachename}
+            with os.fdopen(fd, 'wb') as fh:
+                fh.write(self.request.body)
+            self.finish()
+            juggler.juggle(infile)
+        except Exception as err:
+            self.clear()
+            self.set_status(500)
+            self.finish(error_message(err))
 
 class WSHandler(tornado.websocket.WebSocketHandler):
 
@@ -50,27 +63,34 @@ class WSHandler(tornado.websocket.WebSocketHandler):
         self.write_message(json.dumps(juggler.get_list()))
 
     def on_message(self, message):
-        parsed_json = json.loads(message)
-        if parsed_json['type'] == "link":
-            ydl_opts = {
-            'quiet': "True",
-            'format': 'bestaudio/best'}
-            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                info_dict = ydl.extract_info(parsed_json['link'], download=False)
-                video_title = info_dict.get('title', None)
-                url = info_dict.get("url", None)
-            infile = {'nick':parsed_json['nick'],
-            'filename':video_title,
-            'address':self.request.remote_ip,
-            'mrl':parsed_json['link'],
-            'path':url}
-            juggler.juggle(infile)
-        else:
-            infile = {
+        try:
+            parsed_json = json.loads(message)
+            if parsed_json['type'] == "link":
+                ydl_opts = {
+                'quiet': "True",
+                'format': 'bestaudio/best'}
+                with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                    info_dict = ydl.extract_info(parsed_json['link'], download=False)
+                    video_title = info_dict.get('title', None)
+                    url = info_dict.get("url", None)
+                infile = {'nick':parsed_json['nick'],
+                'filename':video_title,
                 'address':self.request.remote_ip,
-                'mrl':parsed_json['mrl']
-            }
-            juggler.cancel(infile)
+                'mrl':parsed_json['link'],
+                'path':url}
+                juggler.juggle(infile)
+            else:
+                infile = {
+                    'address':self.request.remote_ip,
+                    'mrl':parsed_json['mrl']
+                }
+                juggler.cancel(infile)
+        except Exception as err:
+            self.write_message(json.dumps({
+                'type': 'error',
+                'message': error_message(err)
+            }))
+
 
     def on_close(self):
         print('connection closed')
