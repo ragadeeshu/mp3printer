790308fbabddb8a7dde28a3a606edeb33605912c
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2023-05-09 00:28:04 +0100

    Improve dubstep logic.
    Was generating (unused) random numbers a bit too often.

--- player.py
+++ player.py
@@ -12,9 +12,8 @@ class Player:
         self.vlc_events = self.mediaplayer.event_manager()
         self.vlc_events.event_attach(vlc.EventType.MediaPlayerEndReached, juggler.song_finished, 1)
         self.vlc_events.event_attach(vlc.EventType.MediaPlayerEncounteredError, juggler.song_finished, 1)
-        self._playingDubstep=False
-        self._shouldPlayDubstep=False;
-        self._dubstepPosition=[random.randint(0,2),random.random()]
+        self._playingDubstep = False
+        self._shouldPlayDubstep = (random.randint(0, 1) == 1)
         self._dubstep = [
             "https://www.youtube.com/watch?v=dLyH94jNau0",
             "https://www.youtube.com/watch?v=RRucF7ffPRE",
@@ -26,9 +25,8 @@ class Player:
         self.mediaplayer.stop()
         self.instance.release()
 
-    def handleDubstep(self):
+    def _handleDubstep(self):
         self._playingDubstep = False
-        self._dubstepPosition=[random.randint(0,2),random.random()]
         self._shouldPlayDubstep = not self._shouldPlayDubstep
 
     def _get_link_url(self, link):
@@ -42,7 +40,7 @@ class Player:
 
     def play(self, track):
         try:
-            self.handleDubstep()
+            self._handleDubstep()
             print("Now playing: "+track['filename'])
             path = track['path'] if 'path' in track else self._get_link_url(track['mrl'])
             self.media = self.instance.media_new(path)
@@ -53,7 +51,7 @@ class Player:
             self._juggler.song_finished()
 
     def scratch(self):
-        self.handleDubstep()
+        self._handleDubstep()
         self.mediaplayer.set_media(self._scratch)
         self.mediaplayer.play()
 
@@ -62,16 +60,20 @@ class Player:
 
     def play_fallback(self):
         try:
-            if(self._shouldPlayDubstep):
-                if(self._playingDubstep):
-                    self._dubstepPosition[0]=(self._dubstepPosition[0]+1)%len(self._dubstep)
-                    self._dubstepPosition[1]=0
+            if self._shouldPlayDubstep:
+                if self._playingDubstep:
+                    self._dubstepTrack = (self._dubstepTrack + 1) % len(self._dubstep)
+                    position = 0
+                else:
+                    self._dubstepTrack = random.randint(0, len(self._dubstep) - 1)
+                    position = random.random()
+                self._playingDubstep = True
                 print("Now playing: Dubstep")
-                url = self._get_link_url(self._dubstep[self._dubstepPosition[0]])
+                url = self._get_link_url(self._dubstep[self._dubstepTrack])
                 self.media = self.instance.media_new(url)
                 self.mediaplayer.set_media(self.media)
                 self.mediaplayer.play()
-                self.mediaplayer.set_position(self._dubstepPosition[1])
+                self.mediaplayer.set_position(position)
             else:
                 print("Now playing: Slay radio")
                 self.mediaplayer.set_media(self._fallback)
