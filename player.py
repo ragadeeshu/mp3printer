490f3e4023b15f1fe5943252513e54ce280f4598
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2023-05-13 01:27:59 +0100

    Player refactoring.
    * No reason to create Media objects when MediaPlayer.set_mrl works just
      as well.
    * Prefix all internal attributes with _ (or just make them local
      variables in some cases).
    * Move static media MRLs to class constants.

--- player.py
+++ player.py
@@ -3,27 +3,29 @@ import random
 import yt_dlp
 
 class Player:
+
+    SLAYRADIO = "http://relay3.slayradio.org:8000/"
+    DUBSTEP = [
+        "https://www.youtube.com/watch?v=dLyH94jNau0",
+        "https://www.youtube.com/watch?v=RRucF7ffPRE",
+        "https://www.youtube.com/watch?v=nXaMKZApYDM"
+    ]
+    SCRATCH = "shortscratch.wav"
+
     def __init__(self, juggler):
         self._juggler = juggler
         self.instance = vlc.Instance("--no-video")
-        self.mediaplayer = self.instance.media_player_new()
-        self._fallback = self.instance.media_new("http://relay3.slayradio.org:8000/")
-        self._scratch = self.instance.media_new("shortscratch.wav")
-        self.vlc_events = self.mediaplayer.event_manager()
-        self.vlc_events.event_attach(vlc.EventType.MediaPlayerEndReached, juggler.song_finished, 1)
-        self.vlc_events.event_attach(vlc.EventType.MediaPlayerEncounteredError, juggler.song_finished, 1)
+        self._mediaplayer = self._instance.media_player_new()
+        vlc_events = self._mediaplayer.event_manager()
+        vlc_events.event_attach(vlc.EventType.MediaPlayerEndReached, juggler.song_finished, 1)
+        vlc_events.event_attach(vlc.EventType.MediaPlayerEncounteredError, juggler.song_finished, 1)
         self._playingDubstep = False
         self._shouldPlayDubstep = (random.randint(0, 1) == 1)
-        self._dubstep = [
-            "https://www.youtube.com/watch?v=dLyH94jNau0",
-            "https://www.youtube.com/watch?v=RRucF7ffPRE",
-            "https://www.youtube.com/watch?v=nXaMKZApYDM"
-        ]
         self.play_fallback()
 
     def release(self):
-        self.mediaplayer.stop()
-        self.instance.release()
+        self._mediaplayer.stop()
+        self._instance.release()
 
     def _handleDubstep(self):
         self._playingDubstep = False
@@ -38,49 +40,47 @@ class Player:
             info_dict = ydl.extract_info(link, download=False)
             return info_dict.get("url", None)
 
+    def _play_mrl(self, mrl):
+       self._mediaplayer.set_mrl(mrl)
+       self._mediaplayer.play()
+
     def play(self, track):
         try:
             self._handleDubstep()
             print("Now playing: "+track['filename'])
             path = track['path'] if 'path' in track else self._get_link_url(track['mrl'])
-            self.media = self.instance.media_new(path)
-            self.mediaplayer.set_media(self.media)
-            self.mediaplayer.play()
+            self._play_mrl(path)
         except Exception as err:
             print(err)
             self._juggler.song_finished()
 
     def pause(self):
-        self.mediaplayer.pause()
+        self._mediaplayer.pause()
 
     def scratch(self):
         self._handleDubstep()
-        self.mediaplayer.set_media(self._scratch)
-        self.mediaplayer.play()
+        self._play_mrl(self.SCRATCH)
 
     def get_position(self):
-        return self.mediaplayer.get_position()
+        return self._mediaplayer.get_position()
 
     def play_fallback(self):
         try:
             if self._shouldPlayDubstep:
                 if self._playingDubstep:
-                    self._dubstepTrack = (self._dubstepTrack + 1) % len(self._dubstep)
+                    self._dubstepTrack = (self._dubstepTrack + 1) % len(self.DUBSTEP)
                     position = 0
                 else:
-                    self._dubstepTrack = random.randint(0, len(self._dubstep) - 1)
+                    self._dubstepTrack = random.randint(0, len(self.DUBSTEP) - 1)
                     position = random.random()
                 self._playingDubstep = True
                 print("Now playing: Dubstep")
-                url = self._get_link_url(self._dubstep[self._dubstepTrack])
-                self.media = self.instance.media_new(url)
-                self.mediaplayer.set_media(self.media)
-                self.mediaplayer.play()
-                self.mediaplayer.set_position(position)
+                url = self._get_link_url(self.DUBSTEP[self._dubstepTrack])
+                self._play_mrl(url)
+                self._mediaplayer.set_position(position)
             else:
                 print("Now playing: Slay radio")
-                self.mediaplayer.set_media(self._fallback)
-                self.mediaplayer.play()
+                self._play_mrl(self.SLAYRADIO)
         except Exception as err:
             print(err)
             self._juggler.song_finished()
